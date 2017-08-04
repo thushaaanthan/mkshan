fe0bdfa79e7d4af2685c520a6b504849d56722f5
thushaan <thushaamal93@gmail.com>
2017-08-04 08:46:02 +0530

    this is first update

---
+++ public_html/js/common.js
@@ -0,0 +1,12 @@
+ var fullscreenslider=function()
+  {
+      $("section.main-heading").vegas({
+	delay: 3000,
+    slides: [
+        { src: "images/first.jpg" },
+        { src: "images/second.jpg" },
+        { src: "images/first.jpg" },
+        { src: "images/second.jpg" }
+    ],animation: 'kenburns'
+});
+  }
